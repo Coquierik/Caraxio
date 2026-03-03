25165dfdb7353bd7a1603fbd499865d882f50d01
Coquierik <erikcoq@gmail.com>
2026-03-03 18:45:29 -0300

    Level 2 implementation

--- pythonProject/code/EntityFactory.py
+++ pythonProject/code/EntityFactory.py
@@ -15,9 +15,15 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg': #creating an append to get all the lvl1 img in a simple way
                 list_bg = []
-                for i in range(7):
+                for i in range(7): #Level1Bg img number
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg': #creating an append to get all the lvl1 img in a simple way
+                list_bg = []
+                for i in range(5): #Level2Bg img number
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
